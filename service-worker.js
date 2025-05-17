ac7106c1390387ce1980ea665e4a8b8f24fd6083
yellowbc <yellowbcompany@gmail.com>
2025-05-17 17:29:22 -0300

    Add files via upload
    Atualização completa: PWA com ícones, manifest e service worker

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,21 @@
-self.addEventListener('install', function (event) {
-  console.log('[ServiceWorker] Instalado');
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('yellow-cache').then(function(cache) {
+      return cache.addAll([
+        'index.html',
+        'index.css',
+        'nicepage.css',
+        'jquery.js',
+        'nicepage.js'
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', function (event) {
-  // Pode ser usado para cache futuro
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
 });
